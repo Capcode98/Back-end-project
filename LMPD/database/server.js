9fb4c230a420889ef385303a0672f69ad96c5680
João Luis B. Ribeiro <89806860+Capcode98@users.noreply.github.com>
2024-12-04 23:17:37 -0300

    commit

--- LMPD/database/server.js
+++ LMPD/database/server.js
@@ -11,8 +11,8 @@ import csvParser from 'csv-parser';
 
 const app = express();
 const port = 3000;
-const accountSid = '';
-const authToken = '';
+//const accountSid = '';
+//const authToken = '';
 const client = new twilio(accountSid, authToken);
 
 app.use(cors());
